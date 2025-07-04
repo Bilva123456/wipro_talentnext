762693e535fa0ea429ea5b4ebed4fcb6c11a5811
Bilvarchitha <yadikibilva2005@gmail.com>
2025-07-04 16:19:29 +0530

    Finally added updated Question1.java

--- java_fundamentals/src/java_fundamentals/Question1.java
+++ java_fundamentals/src/java_fundamentals/Question1.java
@@ -3,10 +3,11 @@ package java_fundamentals;
 import java.util.Scanner;
 
 public class Question1 {
-      public static void main(String[]args) {
-    	  Scanner sc = new Scanner(System.in);
-    	  String company = sc.nextLine();
-    	  String location = sc.nextLine();
-    	  System.out.println(company +" Technologies " + location);
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String company = sc.nextLine();
+        String location = sc.nextLine();
+        System.out.println(company + " Technologies " + location);
+    }
 }
+
