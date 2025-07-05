53899cd712caf3cff7aaff3d10f91b667830e15d
Bilva123456 <114167232+Bilva123456@users.noreply.github.com>
2025-07-05 16:27:43 +0530

    Update Question01.java

--- java_fundamentals/src/java_fundamentals/Question01.java
+++ java_fundamentals/src/java_fundamentals/Question01.java
@@ -2,7 +2,7 @@ package java_fundamentals;
 
 import java.util.Scanner;
 
-public class Question1 {
+public class Question01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String company = sc.nextLine();
