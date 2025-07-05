7d72e9414e2fc941553bf30f061242429badeb1e
Bilva123456 <yadikibilva2005@gmail.com>
2025-07-05 15:35:23 +0530

    Question14.java

---
+++ java_fundamentals/src/java_fundamentals/Question14.java
@@ -0,0 +1,9 @@
+package java_fundamentals;
+
+public class Question14 {
+	public static void main(String[] args) {
+        for (int i = 24; i <= 56; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
